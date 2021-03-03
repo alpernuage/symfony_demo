15cfe4d520dc957311c5b282b360276dfeb222de
alpernuage <alperakbulut@gmail.com>
2021-03-03 22:50:45 +0100

    création et sécurisation page login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,9 +18,9 @@ class SecurityController extends AbstractController
         $user = new User();
         
         $form = $this->createForm(RegistrationType::class, $user);
-
+        
         $form->handleRequest($request);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
             $hash = $encoder->encodePassword($user, $user->getPassword());
 
@@ -28,10 +28,18 @@ class SecurityController extends AbstractController
             
             $manager->persist($user);
             $manager->flush();
-        }
 
+            return $this->redirectToRoute('security_login');
+        }
+        
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
-        ]);
+            ]);
+        }
+        
+        
+        #[Route('/connexion', name: 'security_login')]
+        public function login() {
+            return $this->render('security/login.html.twig');
     }
 }
